9e59dea0c4e3b170c1bca7a2c4ead39fac0917cc
kiiyuq <jyli0116@gmail.com>
2024-10-30 00:51:06 +0000

    feat: add initial code

---
+++ build/CMakeFiles/demo_refinable.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/jyli0/General/IMPERIAL/Y3_2425/60007 The Theory and Practise of Concurrency/skeleton/src/benchmark.cc" "CMakeFiles/demo_refinable.dir/src/benchmark.cc.o" "gcc" "CMakeFiles/demo_refinable.dir/src/benchmark.cc.o.d"
+  "/mnt/c/Users/jyli0/General/IMPERIAL/Y3_2425/60007 The Theory and Practise of Concurrency/skeleton/src/demo_refinable.cc" "CMakeFiles/demo_refinable.dir/src/demo_refinable.cc.o" "gcc" "CMakeFiles/demo_refinable.dir/src/demo_refinable.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
