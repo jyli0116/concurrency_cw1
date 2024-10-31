87217b9cecfb1ae14579c481ca7b99a4951e9a1c
jl6922 <jinyu.li22@imperial.ac.uk>
2024-10-31 02:58:46 +0000

    feat: implemented hash_set_sequential, coarse_grained and striped

--- src/hash_set_sequential.h
+++ src/hash_set_sequential.h
@@ -1,34 +1,108 @@
 #ifndef HASH_SET_SEQUENTIAL_H
 #define HASH_SET_SEQUENTIAL_H
 
+#include <stdio.h>
+
 #include <cassert>
+#include <vector>
+#include <functional>
 
 #include "src/hash_set_base.h"
 
 template <typename T>
 class HashSetSequential : public HashSetBase<T> {
- public:
-  explicit HashSetSequential(size_t /*initial_capacity*/) {}
+  public:
+    explicit HashSetSequential(size_t initial_capacity): initial_capacity_(initial_capacity), set_size_(0) {
+      table_.reserve(initial_capacity);
+      for (size_t i = 0; i < initial_capacity; i++) {
+        table_.push_back(std::vector<T>());
+      }
+    }
 
-  bool Add(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
-  }
+    // Adds element to hashset
+    bool Add(T elem) final {
+      std::vector<T>& bucket_ = table_.at(std::hash<T>()(elem) % initial_capacity_);
 
-  bool Remove(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
-  }
+      // check if element is already present in bucket
+      auto bucket_elem_ = bucket_.begin();
+      while (bucket_elem_ != bucket_.end()) {
+        if(*bucket_elem_ == elem) {
+          return false;
+        }
+        ++bucket_elem_;
+      }
 
-  [[nodiscard]] bool Contains(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
-  }
+      // if not add to bucket
+      set_size_++;
+      bucket_.push_back(elem);
 
-  [[nodiscard]] size_t Size() const final {
-    assert(false && "Not implemented yet");
-    return 0u;
-  }
+      if (Policy()) {
+        Resize();
+      }
+
+      return true;
+    }
+
+    // Removes element from hashset
+    bool Remove(T elem) final {
+      if (set_size_ == 0) {
+        return false;
+      }
+
+      std::vector<T>& bucket_ = table_.at(std::hash<T>()(elem) % initial_capacity_);
+
+      // check if element is already present in bucket if so, remove from bucket
+      auto bucket_elem_ = bucket_.begin();
+      while (bucket_elem_ != bucket_.end()) {
+        if(*bucket_elem_ == elem) {
+          bucket_.erase(bucket_elem_);
+          set_size_--;
+          return true;
+        }
+        ++bucket_elem_;
+      }
+
+      return false;
+    }
+
+    // Check if item contains hashset
+    [[nodiscard]] bool Contains(T elem) final {
+      std::vector<T>& bucket_ = table_.at(std::hash<T>()(elem) % initial_capacity_);
+      return std::find(bucket_.begin(), bucket_.end(), elem) != bucket_.end();
+    }
+
+    // Returns size of hashset
+    [[nodiscard]] size_t Size() const final {
+      return set_size_;
+    }
+
+  private: 
+    // Decides wherther to resize the set
+    bool Policy() {
+      return set_size_ / initial_capacity_ > 4;
+    }
+
+    // doubles the capacity of the table
+    void Resize() {
+      // TODO: shorten down to one instruction
+      size_t new_capacity_ = initial_capacity_ * 2;
+      initial_capacity_ = new_capacity_;
+
+      std::vector<std::vector<T>> old_table_ = table_;
+      table_ = std::vector<std::vector<T>>(new_capacity_);
+      for (std::vector<T> bucket : old_table_) {
+        for (T elem : bucket) {
+          std::vector<T>& curr_bucket_ = table_.at(std::hash<T>()(elem) % new_capacity_);
+          curr_bucket_.push_back(elem);
+        }
+      }
+    }
+    
+    // used to represent the Hashset: a vector of vectors of type T: each vector representing the bucket
+    size_t initial_capacity_;
+    size_t set_size_;
+    // idk why my intellisense isn't recognising std here but
+    std::vector<std::vector<T>> table_;
 };
 
 #endif  // HASH_SET_SEQUENTIAL_H
--- src/hash_set_striped.h
+++ src/hash_set_striped.h
@@ -1,34 +1,129 @@
 #ifndef HASH_SET_STRIPED_H
 #define HASH_SET_STRIPED_H
 
+#include <stdio.h>
+
 #include <cassert>
+#include <cstddef>
+#include <vector>
+#include <functional>
+#include <mutex>
 
 #include "src/hash_set_base.h"
 
 template <typename T>
 class HashSetStriped : public HashSetBase<T> {
- public:
-  explicit HashSetStriped(size_t /*initial_capacity*/) {}
+  public:
+    explicit HashSetStriped(size_t initial_capacity) : initial_capacity_(initial_capacity), current_capacity_(initial_capacity) {
+      table_.reserve(initial_capacity);
+      set_size_.store(0);
+      mutexes_ = new std::mutex[initial_capacity];
+      for (size_t i = 0; i < initial_capacity; i++) {
+        table_.push_back(std::vector<T>());
+      }
+    }
 
-  bool Add(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
-  }
 
-  bool Remove(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
-  }
+    bool Add(T elem) final {
+      mutexes_[std::hash<T>()(elem) % initial_capacity_].lock();
+      
+      std::vector<T>& bucket_ = table_.at(std::hash<T>()(elem) % current_capacity_);
 
-  [[nodiscard]] bool Contains(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
-  }
+      // check if element is already present in bucket
+      auto bucket_elem_ = bucket_.begin();
+      while (bucket_elem_ != bucket_.end()) {
+        if(*bucket_elem_ == elem) {
+          mutexes_[std::hash<T>()(elem) % initial_capacity_].unlock();
+          return false;
+        }
+        ++bucket_elem_;
+      }
 
-  [[nodiscard]] size_t Size() const final {
-    assert(false && "Not implemented yet");
-    return 0u;
-  }
+      // if not add to bucket
+      set_size_++;
+      bucket_.push_back(elem);
+
+      // resize needs all threads unlocked prior to calling it
+      mutexes_[std::hash<T>()(elem) % initial_capacity_].unlock();
+
+
+      if (Policy()) {
+        Resize();
+      }
+
+      return true;
+    }
+
+    bool Remove(T elem) final {
+      // since dont need ot unlock this early, could also use scoped_lock?
+      //mutexes_[std::hash<T>()(elem) % initial_capacity_].lock();
+      std::scoped_lock<std::mutex> scoped_lock(mutexes_[std::hash<T>()(elem) % initial_capacity_]);
+
+      if (set_size_.load() == 0) {
+        return false;
+      }
+
+      std::vector<T>& bucket_ = table_.at(std::hash<T>()(elem) % current_capacity_);
+
+      // check if element is already present in bucket if so, remove from bucket
+      auto bucket_elem_ = bucket_.begin();
+      while (bucket_elem_ != bucket_.end()) {
+        if(*bucket_elem_ == elem) {
+          bucket_.erase(bucket_elem_);
+          set_size_--;
+          return true;
+        }
+        ++bucket_elem_;
+      }
+      return false;
+    }
+
+    [[nodiscard]] bool Contains(T elem) final {
+      std::scoped_lock<std::mutex> scoped_lock(mutexes_[std::hash<T>()(elem) % initial_capacity_]);
+
+      std::vector<T>& bucket_ = table_.at(std::hash<T>()(elem) % current_capacity_);
+      
+      return std::find(bucket_.begin(), bucket_.end(), elem) != bucket_.end();
+    }
+
+    [[nodiscard]] size_t Size() const final {
+      return set_size_.load();
+    }
+
+  private: 
+    bool Policy() {
+      return set_size_.load() / current_capacity_ > 4;
+    }
+
+    void Resize() {
+      // lock all mutexes
+      for (size_t i = 0; i < initial_capacity_; i++) {
+        mutexes_[i].lock();
+      }
+      
+      // TODO: shorten down to one instruction
+      size_t new_capacity_ = current_capacity_ * 2;
+      current_capacity_ = new_capacity_;
+
+      std::vector<std::vector<T>> old_table_ = table_;
+      table_ = std::vector<std::vector<T>>(new_capacity_);
+      for (std::vector<T> bucket : old_table_) {
+        for (T elem : bucket) {
+          std::vector<T>& curr_bucket_ = table_.at(std::hash<T>()(elem) % new_capacity_);
+          curr_bucket_.push_back(elem);
+        }
+      }
+
+      for (size_t i = 0; i < initial_capacity_; i++) {
+        mutexes_[i].unlock();
+      }
+    }
+
+    size_t const initial_capacity_;
+    std::atomic<size_t> current_capacity_;
+    std::vector<std::vector<T>> table_;
+    std::mutex* mutexes_;
+    std::atomic<size_t> set_size_;
 };
 
 #endif  // HASH_SET_STRIPED_H
